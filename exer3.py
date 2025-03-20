36267651f141e6b2b235a2d19cf50eb0139c3116
caio-F1 <caiofelipesilva832@gmail.com>
2025-03-20 20:42:34 -0300

    exer3

---
+++ exer3.py
@@ -0,0 +1,8 @@
+nome = input("digite seu nome")
+ano_do_nascimento = int(input("digite seu ano de nascimento"))
+ano_atual = int(input("digite o ano corrente"))
+
+idade = (ano_atual-ano_do_nascimento)
+
+print(nome , "tem ou tera", idade)
+
